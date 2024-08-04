f106e98e6c064368010c2022ac95feee59f2e168
wakaba-kelvin <kimanik918@gmail.com>
2024-08-04 15:03:23 +0300

    setting up the project

---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import navCSS from './../Nav/Nav.module.css'
+
+function Nav() {
+  return (
+    <div className={navCSS.nav}>
+      <h1>Nav</h1>
+    </div>
+  )
+}
+
+export default Nav
---
+++ src/Components/Slider/Slider.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import sliderCSS from './../Slider/Slider.module.css'
+
+function Slider() {
+  return (
+    <div className={sliderCSS.sliderWrapper}>
+      
+    </div>
+  )
+}
+
+export default Slider
